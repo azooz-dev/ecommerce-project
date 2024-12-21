930b98090dd70886b4392c1a9fa310bd3298ce0e
Azooz101ezeo <asooma2005asooma@gmail.com>
2024-12-22 00:34:59 +0200

    add response helper functions

---
+++ app/Helpers/response_functions.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+if (!function_exists('successResponse')) {
+  function successResponse($data, $message = 'Success', $status = 200): JsonResponse
+  {
+    return response()->json([
+      'message' => $message,
+      'data' => $data,
+      'status' => 'success'
+    ], $status);
+  }
+}
+
+if (!function_exists('errorResponse')) {
+  function errorResponse($message = 'Error', $status = 400): JsonResponse
+  {
+    return response()->json([
+      'message' => $message,
+      'status' => 'error'
+    ], $status);
+  }
+}
+
+if (!function_exists('showAll')) {
+  function showAll($collection, $message = 'Success', $status = 200): JsonResponse
+  {
+    return successResponse($collection, $message, $status);
+  }
+}
+
+if (!function_exists('showOne')) {
+  function showOne($model, $message = 'Success', $status = 200): JsonResponse
+  {
+    return successResponse($model, $message, $status);
+  }
+}
